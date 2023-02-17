4270551f4dead8e34a89d141dd3f949184d16e66
alleonam <manoellasdiniz@gmail.com>
2023-02-17 13:19:03 -0300

    Esboço

---
+++ Guia01.cpp
@@ -0,0 +1,130 @@
+/*
+ * Guia01 - v0.1. - 16 / 02 / 2023
+ * Author: Manoella Santos Diniz
+ */
+
+// lista de dependencias
+#include "karel.hpp"
+
+// --------------------------- definicoes de metodos
+
+/**
+ decorateWorld - Metodo para preparar o cenario.
+ @param fileName - nome do arquivo para guardar a descricao.
+*/
+void decorateWorld(const char *fileName)
+{
+    // colocar paredes no mundo
+    world->set(4, 4, HWALL); // horizontal
+    world->set(4, 4, VWALL); // vertical
+
+    // colocar um marcador no mundo
+    world->set(4, 4, BEEPER);
+
+    // salvar a configuracao atual do mundo
+    world->save(fileName);
+}
+
+/**
+ * Classe para definir robo particular (MyRobot),
+ * a partir do modelo generico (Robot)
+ *
+ * Nota: Todas as definicoes irao valer para qualquer outro robo
+ *       criado a partir dessa nova descricao de modelo.
+ */
+class MyRobot : public Robot
+{
+public:
+    /**
+     * turnRight - Procedimento para virar 'a direita.
+     */
+    void turnRight()
+    {
+        // testar se o robo esta' ativo
+        if (checkStatus())
+        {
+            // o agente que executar esse metodo
+            // devera' virar tres vezes 'a esquerda
+            turnLeft();
+            turnLeft();
+            turnLeft();
+        }
+    }
+};
+
+// --------------------------- acao principal
+
+/**
+ * Acao principal: executar a tarefa descrita acima.
+ */
+int main()
+{
+    // definir o contexto
+
+    // criar o ambiente e decorar com objetos
+    // OBS.: executar pelo menos uma vez,
+    //       antes de qualquer outra coisa
+    //       (depois de criado, podera' ser comentado)
+    world->create(""); // criar o mundo
+    decorateWorld("Guia0101.txt");
+    world->show();
+
+    // preparar o ambiente para uso
+    world->reset();              // limpar configuracoes
+    world->read("Guia0101.txt"); // ler configuracao atual para o ambiente
+    world->show();               // mostrar a configuracao atual
+
+    set_Speed(3); // definir velocidade padrao
+
+    // criar robo
+    MyRobot *robot = new MyRobot();
+
+    // posicionar robo no ambiente (situacao inicial):
+    // posicao(x=1,y=1), voltado para direita, com zero marcadores, nome escolhido )
+    robot->create(1, 1, EAST, 0, "Karel");
+
+    // executar tarefa
+    robot->move();
+    robot->move();
+    robot->turnLeft();
+    robot->move();
+    robot->move();
+    robot->turnLeft();
+    robot->move();
+    robot->move();
+    robot->turnLeft();
+    robot->move();
+    robot->move();
+    robot->turnLeft();
+    robot->turnLeft();
+    robot->turnOff();
+
+    // encerrar operacoes no ambiente
+    world->close();
+
+    // encerrar programa
+    getchar();
+    return (0);
+}
+
+// ------------------------------------------- testes
+
+/*
+ *
+ * ---------------------------------------------- documentacao complementar
+ *
+ * ---------------------------------------------- notas / observacoes / comentarios
+ *
+ * ---------------------------------------------- previsao de testes
+ *
+ * ---------------------------------------------- historico
+ *
+ * Versao   Data    Modificacao
+ * 0.1      16/02   esboco
+ *
+ * ---------------------------------------------- testes
+ *
+ * Versao   Teste
+ * 0.1      01. ( OK )  teste inicial
+ *
+ */
